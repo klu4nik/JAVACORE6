cf33d338d15824f3e6f63fc466cb760d258517da
Klu4nik <klu4nik.help@gmail.com>
2017-04-13 22:10:23 +0300

    Homework 5.1 added

---
+++ JAVACORE6/src/main/java/com/andrii/practice/module05/task051/Room.java
@@ -0,0 +1,73 @@
+package main.java.com.andrii.practice.module05.task051;
+
+import java.util.Date;
+
+/**
+ * Created by Klu4nik on 13/04/2017.
+ */
+public class Room {
+    private long id;
+    private int price;
+    private int persons;
+    private Date dateAvailableFrom;
+    private String hotelName;
+    private String cityName;
+
+    public Room(long id, int price, int persons, Date dateAvailableFrom, String hotelName, String cityName) {
+        this.id = id;
+        this.price = price;
+        this.persons = persons;
+        this.dateAvailableFrom = dateAvailableFrom;
+        this.hotelName = hotelName;
+        this.cityName = cityName;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setPersons(int persons) {
+        this.persons = persons;
+    }
+
+    public void setDateAvailableFrom(Date dateAvailableFrom) {
+        this.dateAvailableFrom = dateAvailableFrom;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getPersons() {
+        return persons;
+    }
+
+    public Date getDateAvailableFrom() {
+        return dateAvailableFrom;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+}

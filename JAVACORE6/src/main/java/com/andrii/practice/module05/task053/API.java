8f6c4890e82aef6ff5fad57ce3cb86af6ab41ab6
Klu4nik <klu4nik.help@gmail.com>
2017-04-23 20:16:26 +0300

    Homework 5.3 added

--- JAVACORE6/src/main/java/com/andrii/practice/module05/task053/API.java
+++ JAVACORE6/src/main/java/com/andrii/practice/module05/task053/API.java
@@ -8,5 +8,5 @@ import main.java.com.andrii.practice.module05.task051.Room;
 public interface API {
     Room[] findRooms(int price, int persons, String city, String hotel);
 
-    Room[] getRooms();
+    Room [] getDB ();
 }

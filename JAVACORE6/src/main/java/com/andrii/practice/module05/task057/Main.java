2c16331b3e1b64fb15a499a8106a5c2b04656248
klu4nik <klu4nik.help@gmail.com>
2017-04-24 18:12:05 +0300

    Add homework 5.7

---
+++ JAVACORE6/src/main/java/com/andrii/practice/module05/task057/Main.java
@@ -0,0 +1,13 @@
+package main.java.com.andrii.practice.module05.task057;
+
+import main.java.com.andrii.practice.module05.task056.Controller;
+
+/**
+ * Created by Unicsoft on 24/04/2017.
+ */
+public class Main {
+    public static void main(String Args[]){
+        Controller controller = new Controller();
+        controller.requestRooms(780, 2, "Leotel", "Lviv");
+    }
+}

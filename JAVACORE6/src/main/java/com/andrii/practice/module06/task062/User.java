1ad3772ae9cf000cd19269bd71d313ac179771e7
Klu4nik <klu4nik.help@gmail.com>
2017-04-25 21:03:01 +0300

    Task 062 added

---
+++ JAVACORE6/src/main/java/com/andrii/practice/module06/task062/User.java
@@ -0,0 +1,73 @@
+package main.java.com.andrii.practice.module06.task062;
+
+/**
+ * Created by Klu4nik on 25/04/2017.
+ */
+public final class User {
+    private final long id;
+    private final String firstName;
+    private final String lastName;
+    private final int salary;
+    private final int balance;
+
+    public User(long id, String firstName, String lastName, int salary, int balance) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+        this.balance = balance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (salary != user.salary) return false;
+        if (balance != user.balance) return false;
+        if (!firstName.equals(user.firstName)) return false;
+        return lastName.equals(user.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        result = 31 * result + salary;
+        result = 31 * result + balance;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                ", balance=" + balance +
+                '}';
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}

eea8758808db74c6e32cb3d26de56d96137b220e
Klu4nik <klu4nik.help@gmail.com>
2017-05-02 20:48:27 +0300

    task082 added

--- JAVACORE6/src/main/java/com/andrii/practice/module08/task081/IdGenerator.java
+++ JAVACORE6/src/main/java/com/andrii/practice/module08/task081/IdGenerator.java
@@ -4,11 +4,11 @@ package main.java.com.andrii.practice.module08.task081;
  * Created by Klu4nik on 02/05/2017.
  */
 public final class IdGenerator {
-    public static final long createId(String country) {
-        int result = country.hashCode();
-        result = 31 * result;
-
-        return result;
+    public static final long createId(String name, Country country, int expiration) {
+        int id = name.hashCode();
+        id = 31 * id + country.hashCode();
+        id = 31 * id + expiration;
+        return id;
 
     }
 }
---
+++ JAVACORE6/src/main/java/com/andrii/practice/module08/task082/Food.java
@@ -0,0 +1,56 @@
+package main.java.com.andrii.practice.module08.task082;
+
+import main.java.com.andrii.practice.module08.task081.Country;
+import main.java.com.andrii.practice.module08.task081.IdGenerator;
+
+/**
+ * Created by Klu4nik on 02/05/2017.
+ */
+public class Food {
+    private static long id;
+    private String name;
+    private Country country;
+    private int expiration;
+
+    public Food(String name, Country country, int expiration) {
+        this.id = IdGenerator.createId(name, country, expiration);
+        this.name = name;
+        this.country = country;
+        this.expiration = expiration;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Food food = (Food) o;
+
+        if (id != food.id) return false;
+        return country == food.country;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + country.hashCode();
+        result = 31 * result + expiration;
+        return result;
+    }
+
+    public static long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public int getExpiration() {
+        return expiration;
+    }
+}

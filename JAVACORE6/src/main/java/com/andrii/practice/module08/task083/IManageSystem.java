d5522bc3a54719c4dc15fab8ed418793ee797094
Klu4nik <klu4nik.help@gmail.com>
2017-05-02 20:52:31 +0300

    task083 added

---
+++ JAVACORE6/src/main/java/com/andrii/practice/module08/task083/IManageSystem.java
@@ -0,0 +1,25 @@
+package main.java.com.andrii.practice.module08.task083;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by Klu4nik on 02/05/2017.
+ */
+public interface IManageSystem<T> {
+    T save(T obj, double price);
+
+    T save(T obj);
+
+    void delete(T obj);
+
+    void deleteById(int id);
+
+    T get(int id);
+
+    Double getPrice(T obj);
+
+    Set<T> getProducts();
+
+    List<Double> getPrices();
+}
